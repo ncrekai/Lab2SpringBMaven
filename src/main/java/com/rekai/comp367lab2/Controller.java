000179b4009112e425681752a94bddbfd5bdce80
Natalie Rekai <nrekai@my.centennialcollege.ca>
2025-03-12 22:23:28 -0400

    Update app display

--- src/main/java/com/rekai/comp367lab2/Controller.java
+++ src/main/java/com/rekai/comp367lab2/Controller.java
@@ -1,4 +1,8 @@
 package com.rekai.comp367lab2;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +12,16 @@ public class Controller {
 
    @GetMapping("/")
    public String index() {
-      return "Welcome to COMP367";
+      LocalTime currentTime = LocalTime.now();
+      LocalTime noon = LocalTime.NOON;
+      boolean afternoon = currentTime.isAfter(noon);
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a");
+      
+      String dayTime = afternoon ? "afternoon" : "morning";
+      String greeting = String.format("Good %s, Natalie. Welcome to COMP367. \n\n", dayTime);
+      String currenTimetDisplay = String.format("Current time is: %s", currentTime.format(formatter));
+
+      return greeting + currenTimetDisplay;
    }
 }
 
